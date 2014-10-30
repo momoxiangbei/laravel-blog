7c8eaea377b63263476965651fc1d62f44dda159
momoxiangbei <1414058425@qq.com>
2014-10-30 13:25:14 +0800

    delete test

--- app/routes.php
+++ app/routes.php
@@ -1,17 +1,5 @@
 <?php
 
-#------------------test------------------
-// Route::get('1', 'TestController@index');
-// Route::get('11', function(){
-// 	return View::make('test');
-// });
-Route::get('11', [
-	'as' => 'test',
-	'uses' => 'TestController@index'
-]);
-Route::resource('test2','test2Controller');
-
-
 
 # -----------------pages-----------------
 
